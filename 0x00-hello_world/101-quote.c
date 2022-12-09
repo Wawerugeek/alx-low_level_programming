adaef29d5fc0fb41545e1e6eb52450b5af923760
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-09 01:15:12 +0000

    when

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	write(2, "and that place of art is useful \" - Dora Korpar, 2015-10-19\n", 61i);
+	write(2, "and that place of art is useful \" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
