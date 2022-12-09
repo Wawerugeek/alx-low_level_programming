709150d64daf90834c587e013068243df0fea582
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-09 01:22:58 +0000

    jkjk

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -Entry point
+ * main - prints exactly and that place of art is useful
  *
  * Return: Always 0  (Success)
  */
 int main(void)
 {
-	write(2, "and that place of art is useful \" - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that place of art is useful \" - Dora Korpar, 2015-10-19\n", 59);
 
 
 	return (1);
