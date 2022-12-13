70c99414ec01f8ac6a5546ac07e807592106276c
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-13 12:00:36 +0000

    changes

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 10; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		if (num == 9)
+		if (num == '9')
 		continue;
 		putchar(',');
 		putchar(' ');
