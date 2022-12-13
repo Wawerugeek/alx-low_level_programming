1394c540c74661b76d83d0859333bb0ac766c8e2
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-13 09:06:23 +0000

    No function

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* header files goes here*/
+
+/**
+  * Main - Entry point
+  *
+  *
+  *Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
