780f12d9e9dfb1fa46d15e8f414cbc1fd565cd9a
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-13 21:48:44 +0000

    Hard anf

--- 0x01-variables_if_else_while/102_print_comb5.c
+++ 0x01-variables_if_else_while/102_print_comb5.c
@@ -15,13 +15,13 @@ int main(void)
 		{
 			if(num2 > num1)
 			{
-				putchar(num1 / 10 + '0');
-				putchar(num1 % 10 + '0');
+				putchar(num1 / 10 + 48);
+				putchar(num1 % 10 + 48);
 				putchar(' ');
-				putchar(num2 / 10 + '0');
-				putchar(num2 % 10 + '0');
+				putchar(num2 / 10 + 48);
+				putchar(num2 % 10 + 48);
 
-				if (num1 * 100 + num2 != 9899)
+				if (num1 * 98 + num2 != 99)
 				{
 					putchar(',');
 					putchar(' ');
