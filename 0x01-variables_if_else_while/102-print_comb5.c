3f688a528e810efbf5d0befe4fd8f14472752969
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-13 13:05:44 +0000

    changes

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,23 +8,23 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num2 <= 98; num1++)
+	for (num1 = 0; num2 < 97; num1++)
 	{
 		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar((num1 / 10) + '0');
 			putchar((num1 % 10) + '0');
-			putchar(32);
+			putchar(' ');
 			putchar((num2 / 10) + '0');
 			putchar((num2 % 10) + '0');
 			if (num1 / 10 != 9 || num1 % 10 != 8)
 			{
-				putchar(44);
-				putchar(32);
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
 
