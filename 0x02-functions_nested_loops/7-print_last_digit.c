3f9e1845b4082ebcc8112c2c24786bd494e22d0b
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-14 23:05:32 +0000

    message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,14 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+	int lst;
+
+	lst = n % 10;
+
+	if (lst < 0)
+	{
+		lst = lst * -1;
+	}
+	_putchar(lst + '0');
+	return (lst);
 }
