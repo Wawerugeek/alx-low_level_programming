ccf55607edb49ad31a57a6ef1ae254069a33177c
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-14 20:06:12 +0000

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_alphabet - print alphabets in lowercase
+ */
+void print_alphabet(void);
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'b'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
