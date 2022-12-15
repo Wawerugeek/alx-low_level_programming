a5d324b0b0b1b9c48e8c0f550bc7d0e8f4be4741
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-15 00:01:55 +0000

    Goodnight

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,12 @@ void print_to_98(int n)
 	{
 		for (n = n; n < 98; n++)
 			printf("%d, ", n);
-		printf("%d \n", 98);
+		printf("%d\n", 98);
 	}
 	else
 	{
 		for (n = n; n > 98; n--)
 			printf("%d, ", n);
-		printf("%d \n", 98);
+		printf("%d\n", 98);
 	}
 }
