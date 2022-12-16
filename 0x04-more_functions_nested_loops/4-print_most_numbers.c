7ca3ae5ff3583747c544231805f7ef71a2c94f47
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-16 00:34:10 +0000

    exclude 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,14 +5,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i < 10)
 	{
-		if (i == 2 && i == 4)
-			continue;
-		else
+		if (i != 2 && i != 4)
 			_putchar(i + '0');
+		i++;
 	}
 	_putchar('\n');
 }
