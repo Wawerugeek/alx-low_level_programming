34d60dda49e04d9b157946fb3df656f0efe9b55a
Wawerugeek <Stevemuiruri84@gmail.com>
2022-12-17 01:45:20 +0000

    stll

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,21 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && a >= c)
+	if (a > b && a > c)
 	{
 		largest = a;
 	}
-	else if (b >= c && b >= c)
+	else if (b > c && b > c)
 	{
 		largest = b;
 	}
-	else
+	else if (c > b)
 	{
 		largest = c;
+	}
+	else
+	{
+		largest = b;
 	}
 	return (largest);
 }
