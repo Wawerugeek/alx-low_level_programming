7f0b5f281d422d5eb3010ac23d055533c49b0191
Wawerugeek <Stevemuiruri84@gmail.com>
2023-01-05 18:59:44 +0000

    changes

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- *_strlen_recursion: returns the string length 
- *@s: String to be returned 
+ *_strlen_recursion - returns the string length
+ *@s: String to be returned
  *Return: on sucess 1
  */
 int _strlen_recursion(char *s)
