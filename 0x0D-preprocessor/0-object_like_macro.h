cf88b5fa6f834e8fa3a1bbe4d63a92faf9338c63
Wawerugeek <Stevemuiruri84@gmail.com>
2023-01-17 20:05:27 +0000

    ipdates

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,4 +1,6 @@
-#ifndef SIZE_H
-#define SIZE_H
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
 #define SIZE 1024
+
 #endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include "3-function_like_macro.h"
+#include <stdio.h>
+#include "4-sum.h"
+#include "0-object_like_macro.h"
+ _putchar(char c);
+#endif
+
