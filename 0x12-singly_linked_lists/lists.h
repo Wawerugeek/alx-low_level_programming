1087c48b9bf1186a9445cba9e883c129cfa9fe3c
Wawerugeek <stevemuiruri84@gmail.com>
2023-01-30 20:30:43 +0000

    change

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,13 @@
 #ifndef LISTS_H
 #define LISTS_H
 /**
-   * struct list_s - singly linked list
-    * @str: string - (malloc'ed string)
-     * @len: length of the string
-      * @next: points to the next node
-       *
-        * Description: singly linked list node structure
-	 */
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
