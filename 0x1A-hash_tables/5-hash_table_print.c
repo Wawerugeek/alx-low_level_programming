7d87b1eba37643484aa8826bc793bc09edad5ea9
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-10 17:12:06 +0300

    print hashtable

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints the table
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node1;
+	char *c_flag = "";
+	unsigned long int x;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			node1 = ht->array[x];
+			while (node1)
+			{
+				printf("'%s''%s': '%s'", c_flag, node1->key, node1->value);
+				c_flag = ", ";
+				node1 = node1->next;
+			}
+		}
+	}
+	printf("}\n");
+}
+
