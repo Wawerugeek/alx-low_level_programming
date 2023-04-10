c775635c6ab9473b4ab22f7b29e48cc006531696
Wawerugeek <stevemuiruri84@gmail.com>
2023-04-10 17:33:56 +0300

    delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - function delest the hash_table
+ * @ht: hash table
+ * Return: Void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int x;
+	hash_node_t *node1;
+
+	for (x = 0; x < ht->size; x++)
+	{
+		if (ht->array[x] != NULL)
+		{
+			while (ht->array[x] != NULL)
+			{
+				node1 = ht->array[x]->next;
+				free(ht->array[x]->key);
+				free(ht->array[x]->value);
+				ht->array[x] = NULL;
+				ht->array[x]->value = NULL;
+				free(ht->array[x]);
+				ht->array[x] = node1;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
